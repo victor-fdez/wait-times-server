e0d8463e1423bca0c8bf85c619070d0e1ba813ec
victor-fdez <victor.j.fdez@gmail.com>
2014-03-10 14:01:23 -0600

    build script for openlayers minimization

---
+++ WaitTimes/static/libs/build_openlayers.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#add compiler javascript to tools
+cd openlayers/tools/
+curl -O http://dl.google.com/closure-compiler/compiler-latest.zip
+unzip -o compiler-latest.zip
+rm -f compiler-latest.zip
+mv -f compiler.jar closure-compiler.jar
+
+cd ../build
+python build.py -c closure full OpenLayers-closure.js
+cd ../../
+mkdir -p openlayers.min/
+mv openlayers/build/OpenLayers-closure.js openlayers.min/OpenLayers.js
+
